0d3190f788727daf6eb7e5156d680959c2737bf9
Rohit Sharma <boy108zon@users.noreply.github.com>
2019-06-20 18:13:41 +0530

    Added Dropzon js for file upload
    I have  added dropzon js for uploading file in step form

--- controllers/Form.php
+++ controllers/Form.php
@@ -53,25 +53,18 @@ class Form extends MY_Controller {
 
     public function create_new_form() {
 
-        $data['meta_title'] = 'Test | Create new Form';
-        $data['meta_keywords'] = 'Test | Create new Form';
-        $data['meta_desc'] = 'Test | Create new Form';
-
-
+        $data['meta_title'] = 'True Startup | Create new Form';
+        $data['meta_keywords'] = 'True Startup | Create new Form';
+        $data['meta_desc'] = 'True Startup | Create new Form';
         $form_process_id = (int) $this->input->get('fpi');
         $is_previous_action = $this->input->get('p');
-
         $form_id = $this->uri->segment(4, 0);
         $step_id_u = $this->uri->segment(5, 1);
-
         $step_id_p = isset($_POST['step_id']) ? (int) $_POST['step_id'] + 1 : $step_id_u;
-
         $this->load->model('common');
         $this->load->model('CustomForms');
-
         $total_steps = $this->CustomForms->get_total_form($form_id);
         $form_name_info = $this->CustomForms->get_form_name($form_id);
-        
         if ($step_id_p != '') {
             if ($is_previous_action != '') {
                 $field_id = (int) $step_id_p;
@@ -81,7 +74,7 @@ class Form extends MY_Controller {
         } else {
             $field_id = $step_id_u;
         }
-      
+
         $form_info = $this->CustomForms->get_step_from($form_id, $field_id);
         $form_data = $this->CustomForms->get_from_data($form_process_id, $field_id);
 
@@ -119,11 +112,11 @@ class Form extends MY_Controller {
         $data['next_step'] = $form_info[0]->field_id;
         $data['actual_step_id_p'] = $step_id_p;
         $data['post_id'] = $save_id;
-        $data['form_name_info'] =$form_name_info;
+        $data['form_name_info'] = $form_name_info;
         $data['header'] = 'admin/template/header';
         $data['sidebar'] = 'admin/template/sidebar';
         $data['main'] = 'admin/stepforms/' . $form_id . '/' . $form_info[0]->step;
-        $data['custom_file_upload'] = 'admin/stepforms/custom_file_upload';
+        $data['custom_file_upload'] = 'admin/stepforms/custom_file_upload_mp';
         $data['footer'] = 'admin/template/footer';
         $this->load->vars($data);
         $this->load->view($this->admin_dashboard);
