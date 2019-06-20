5ba7068585a662d53a09014577d870d071968508
Rohit Sharma <boy108zon@users.noreply.github.com>
2019-06-20 18:21:44 +0530

    Model Pop View
    Added Model Pop For All Step Forms

---
+++ views/stepforms/custom_file_upload_mp.php
@@ -0,0 +1,6 @@
+<div class="modal fade" tabindex="-1" role="dialog" id="myaddcustomebanner" data-backdrop="false">
+    <div class="modal-dialog">
+        <div class="modal-content">
+        </div>
+    </div>
+</div>
