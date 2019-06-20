8f277f2f597b37085857c3f12f47c246da9e42c7
Rohit Sharma <boy108zon@users.noreply.github.com>
2019-06-20 18:17:12 +0530

    Add Dropzon js , Move out model content
    Added Dropzon js  , move out model pop

--- views/stepforms/custom_file_upload.php
+++ views/stepforms/custom_file_upload.php
@@ -1,5 +1,6 @@
-<link href="<?php echo base_url(); ?>assets/dist/uploader/uploadfile.css" rel="stylesheet">
-<script src="<?php echo base_url(); ?>assets/dist/uploader/jquery.uploadfile.js"></script>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.5.1/basic.css" rel="stylesheet">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.5.1/dropzone.css" rel="stylesheet">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.5.1/min/dropzone.min.js"></script>
 <style>
     #myOverlay {height:100%;width:100%;position:absolute;top:0px;left:0px;}
     #myOverlay {background:black;opacity:0.5;z-index:10;display:none;}
@@ -14,112 +15,236 @@
         font-size:14px;
         font-weight: normal;
     }
+    /* Mimic table appearance */
+    .container1{
+        width: 100%;
+    }
+    .container1{
+        padding-right: 15px;
+        padding-left: 15px;
+        margin-right: auto;
+        margin-left: auto;
+    }
+    .table{
+        width: 100%;
+        margin: 0px;
+    }
+    .progress-bar{
+        width: 48%;
+    }
+    div.table {
+        display: table;
+    }
+    div.table .file-row {
+        display: table-row;
+    }
+    div.table .file-row > div {
+        display: table-cell;
+        vertical-align: top;
+        border-top: 1px solid #ddd;
+        padding: 8px;
+    }
+    div.table .file-row:nth-child(odd) {
+        background: #f9f9f9;
+    }
+    /* The total progress gets shown by event listeners */
+    #total-progress {
+        opacity: 0;
+        transition: opacity 0.3s linear;
+    }
+    /* Hide the progress bar when finished */
+    #previews .file-row.dz-success .progress {
+        opacity: 0;
+        transition: opacity 0.3s linear;
+    }
+    /* Hide the delete button initially */
+    #previews .file-row .delete {
+        display: none;
+    }
+    /* Hide the start and cancel buttons and show the delete button */
+    #previews .file-row.dz-success .start,
+    #previews .file-row.dz-success .cancel {
+        display: none;
+    }
+    #previews .file-row.dz-success .delete {
+        display: block;
+    }
+
 </style>
 <div id="myOverlay"></div>
-<div class="modal fade" tabindex="-1" role="dialog" id="myaddcustomebanner" data-backdrop="false">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">×</span></button>
-                <h4 class="modal-title">Upload A Document</h4>
+<div class="modal-header">
+    <button type="button" class="close myclosebtnmp" data-dismiss="modal" aria-label="Close">
+        <span aria-hidden="true">X</span></button>
+    <h4 class="modal-title">Upload A Document</h4>
+</div>
+<div class="modal-body">
+    <div class="faster-ajax-loader faster_ajax_loader" style="display:none;"></div>
+    <div class="popup_message"></div>
+    <div class="container1">
+        <input type="hidden" name="m_form_id" id="m_form_id" value="<?php echo isset($form_id) ? $form_id : ''; ?>"/>
+        <input type="hidden" name="m_form_process_id" id="m_form_process_id" value="<?php echo isset($form_process_id) ? $form_process_id : ''; ?>"/>
+        <input type="hidden" name="m_step_id" id="m_step_id" value="<?php echo isset($step_id) ? $step_id : ''; ?>"/>
+        <input type="hidden" name="m_doc_key" id="m_doc_key" value="<?php echo isset($doc_key) ? $doc_key : ''; ?>"/>
+
+        <div id="actions" class="row">
+            <div class="col-lg-12">
+                <span class="btn btn-success fileinput-button dz-clickable">
+                    <i class="glyphicon glyphicon-plus"></i>
+                    <span>Add files...</span>
+                </span>
+                <button type="submit" class="btn btn-primary start">
+                    <i class="glyphicon glyphicon-upload"></i>
+                    <span>Start upload</span>
+                </button>
+                <button type="reset" class="btn btn-warning cancel">
+                    <i class="glyphicon glyphicon-ban-circle"></i>
+                    <span>Cancel upload</span>
+                </button>
             </div>
-            <div class="modal-body">
-                <input type="hidden" name="m_form_id" id="m_form_id" value=""/>
-                <input type="hidden" name="m_form_process_id" id="m_form_process_id" value=""/>
-                <input type="hidden" name="m_step_id" id="m_step_id" value=""/>
-                <input type="hidden" name="m_doc_key" id="m_doc_key" value=""/>
-                
-                <div class="faster-ajax-loader faster_ajax_loader" style="display:none;"></div>
-                <div class="popup_message"></div>
-                <div class="b-form-row">
-                        <div id="mulitplefileuploader">Browse</div>
-                </div>
-                <div class="document-container">
-                    
-                </div>
+
+            <div class="col-lg-5">
+                <!-- The global file processing state -->
+                <span class="fileupload-process">
+                    <div id="total-progress" class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0">
+                        <div class="progress-bar progress-bar-success" style="width:0%;" data-dz-uploadprogress=""></div>
+                    </div>
+                </span>
             </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-danger pull-left">Allowed Images: pdf,doc,docx  Max Size: 5 MB</button>
+        </div>
+        <div class="table table-striped files" id="previews">
+            <div id="template" class="file-row dz-image-preview">
+                <!-- This is used as the file preview template -->
+                <div>
+                    <span class="preview"><img data-dz-thumbnail></span>
+                </div>
+                <div>
+                    <p class="name" data-dz-name></p>
+                    <strong class="error text-danger" data-dz-errormessage></strong>
+                </div>
+                <div>
+                    <p class="size" data-dz-size></p>
+                    <div class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0">
+                        <div class="progress-bar progress-bar-success" style="width:0%;" data-dz-uploadprogress></div>
+                    </div>
+                </div>
+                <div>
+                    <button class="btn btn-primary start">
+                        <i class="glyphicon glyphicon-upload"></i>
+                        <span>Start</span>
+                    </button>
+                    <button data-dz-remove class="btn btn-warning cancel">
+                        <i class="glyphicon glyphicon-ban-circle"></i>
+                        <span>Cancel</span>
+                    </button>
+                    <button data-dz-remove class="btn btn-danger delete">
+                        <i class="glyphicon glyphicon-trash"></i>
+                        <span>Delete</span>
+                    </button>
+                </div>
             </div>
         </div>
     </div>
 </div>
+<div class="modal-footer">
+    <button type="button" id="submit-all" class="btn btn-danger pull-left">Allowed Images: pdf,doc,docx  Max Size: 5 MB</button>
+</div>
 <script>
-
     $(document).ready(function () {
-        var settings = {
-            url: "<?php echo base_url() . 'admin/form/do_upload_form_document'; ?>",
-            method: "POST",
-            allowedTypes: "pdf,doc,docx",
-            fileName: "userfile",
-            showCancel: true,
-            dragDrop: true,
-            showAbort: true,
-            autoSubmit: true,
-            abortStr: true,
-            showDone: false,
-            cancelStr: "Cancel",
-            multiple: false,
-            returnType: "json",
-            showDelete: true,
-            dynamicFormData: function () {
-                var m_fid = document.getElementById("m_form_id");
-                var m_form_id = m_fid.value;
-                
-                var m_fpid = document.getElementById("m_form_process_id");
-                var m_fp_id = m_fpid.value;
-                
-                var m_sid = document.getElementById("m_step_id");
-                var m_s_id = m_sid.value;
-                
-                var m_dk = document.getElementById("m_doc_key");
-                var m_dkey = m_dk.value;
-                
-                
-                var data = {m_form_id: m_form_id,m_form_process_id:m_fp_id,m_step_id:m_s_id,m_doc_key:m_dkey}
-                return data;
-            },
-            deleteCallback: function (data, pd) {
-                for (var i = 0; i < data.length; i++) {
-                    $.post("<?php echo base_url() . 'admin/form/do_remove_form_document'; ?>", {op: "remove", bid: encodeURIComponent(data[i])},
-                    function (resp, textStatus, jqXHR) {
-                        $(".popup_message").html('<div class="showmessage_jscbf_File alert alert-success">Removed successfully</div>');
-                        $(".showmessage_jscbf_File").fadeOut("slow");
+        var previewNode = document.querySelector("#template");
+        previewNode.id = "";
+        var previewTemplate = previewNode.parentNode.innerHTML;
+        previewNode.parentNode.removeChild(previewNode);
+        var myDropzoneOut='';
+        $("#myaddcustomebanner").on("shown.bs.modal", function () {
+           var myDropzone = new Dropzone(".container1", {
+                url: "<?php echo base_url() . 'admin/form/do_upload_form_document'; ?>",
+                thumbnailWidth: 80,
+                thumbnailHeight: 80,
+                parallelUploads: 1,
+                addRemoveLinks: false,
+                previewTemplate: previewTemplate,
+                autoQueue: false,
+                previewsContainer: "#previews",
+                clickable: ".fileinput-button",
+                acceptedFiles: 'application/pdf,application/xls,application/excel,application/vnd.ms-excel,application/vnd.ms-excel; charset=binary,application/msexcel,application/x-excel,application/x-msexcel,application/x-ms-excel,application/x-dos_ms_excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                init: function () {
+                    $('.dz-image-preview').remove();
+                    thisDropzone = this;
+                    thisDropzone.on('maxfilesexceeded', function (file) {
+                        thisDropzone.removeAllFiles();
+                        thisDropzone.addFile(file);
+                    });
+                    var file = {};
+                    thisDropzone.on("success", function (file, response) {
+                        var resC = JSON.parse(response);
+                        file['name'] = resC.name;
+                        file['size'] = resC.size;
+                        file['id'] = resC.id;
+                    });
+                    thisDropzone.on("removedfile", function (file, response) {
+                        $.ajax({
+                            url: '<?php echo base_url() . 'admin/form/do_remove_form_document/'; ?>',
+                            type: "POST",
+                            dataType: 'json',
+                            data: {'bid': file.id, op: "remove"},
+                            success: function (response) {
+                                $(".popup_message").html(response.msg);
+                                $(".showmessage_jscbf_File").fadeOut("slow");
+                            }
+                        });
+                    });
+
+                    $.ajax({
+                        url: '<?php echo base_url() . 'admin/form/list_all'; ?>',
+                        type: 'POST',
+                        dataType: 'json',
+                        data: {'form_id': encodeURIComponent($("#m_form_id").val()), 'form_process_id': encodeURIComponent($("#m_form_process_id").val()), 'step_id': encodeURIComponent($("#m_step_id").val()), 'doc_key': encodeURIComponent($("#m_doc_key").val())},
+                        success: function (mockFile) {
+                            if (mockFile.length > 0) {
+                                $.each(mockFile, function (key, value) {
+                                    var mockFile = {name: value.name, size: value.size, id: value.doc_id}
+                                    mockFile.accepted = true;
+                                    thisDropzone.emit('addedfile', mockFile)
+                                    thisDropzone.emit('thumbnail', mockFile, 'http://someserver.com/myimage.jpg')
+                                    thisDropzone.emit('complete', mockFile)
+                                    mockFile.previewElement.classList.add('dz-success');
+                                    mockFile.previewElement.classList.add('dz-complete');
+                                });
+                            }
+                        }
                     });
                 }
-                pd.statusbar.hide(); //You choice.
-            },
-            onSelect: function (files) {
-
-                return true; //to allow file submission.
-            },
-            onSuccess: function (files, data, xhr) {
-                switch (data.Mstatus) {
-                    case 'success':
-                        $(".popup_message").html("");
-                        $(".popup_message").html('<div class="alert alert-success">Uploaded successfully</div>');
-                        break;
-                    case 'error':
-                        $(".popup_message").html(data.msg);
-                        break;
-                    default:
-                        break;
-                }
-            },
-            afterUploadAll: function () {
-
-            },
-            onError: function (files, status, errMsg) {
-                alert(errMsg);
-            }
-        }
-        var uploadObj = {}
-        $('#myaddcustomebanner').on('shown.bs.modal', function () {
-            uploadObj = $("#mulitplefileuploader").uploadFile(settings);
-        })
+            });
+            myDropzoneOut=myDropzone;
+            myDropzone.on("addedfile", function (file) {
+                file.previewElement.querySelector(".start").onclick = function () {
+                    myDropzone.enqueueFile(file);
+                };
+            });
+            myDropzone.on("totaluploadprogress", function (progress) {
+                document.querySelector("#total-progress .progress-bar").style.width = progress + "%";
+            });
+            myDropzone.on("sending", function (file, xhr, formData) {
+                document.querySelector("#total-progress").style.opacity = "1";
+                formData.append("m_form_id", $("#m_form_id").val());
+                formData.append("m_form_process_id", $("#m_form_process_id").val());
+                formData.append("m_step_id", $("#m_step_id").val());
+                formData.append("m_doc_key", $("#m_doc_key").val());
+                file.previewElement.querySelector(".start").setAttribute("disabled", "disabled");
+            });
+            myDropzone.on("queuecomplete", function (progress) {
+                document.querySelector("#total-progress").style.opacity = "0";
+            });
+            document.querySelector("#actions .start").onclick = function () {
+                myDropzone.enqueueFiles(myDropzone.getFilesWithStatus(Dropzone.ADDED));
+            };
+            document.querySelector("#actions .cancel").onclick = function () {
+                myDropzone.removeAllFiles(true);
+            };
+        }).modal('show');
         $('#myaddcustomebanner').on('hidden.bs.modal', function () {
-            uploadObj.remove();
-        })
+            //myDropzone.removeAllFiles(true);
+            //myDropzoneOut.off();
+        });
     });
 </script>
